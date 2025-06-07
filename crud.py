161b4af5bf05b049ce2a9ad39ffcbc4b5559b71c
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-06-07 21:40:17 +0300

    shit

--- crud.py
+++ crud.py
@@ -5,6 +5,18 @@ from message import Message
 
 
 def create_message(message: Message) -> Message:
+    message = Message(
+        spam_score=message.spam_score,
+        text=message.text,
+        date=message.date,
+        chat_id=10,
+        chat_type=message.chat_type,
+        user_id=message.user_id,
+        user_username=message.user_username,
+        user_first_name=message.user_first_name,
+        user_last_name=message.user_last_name,
+        reply_to_message_id=message.reply_to_message_id,
+    )
     with Session(engine) as session:
         session.add(message)
         session.commit()
